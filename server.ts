61734b4e17953279668ae10664e0661d8e492c24
pageshih <a0981863141@gmail.com>
2023-03-29 11:55:19 +0800

    add mongoDB enviroment

--- server.ts
+++ server.ts
@@ -1,21 +1,23 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
-
-const port = parseInt(process.env.PORT || '3000', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
+import { MongoClient } from 'mongodb';
+const uri =
+  'mongodb+srv://PageShih:qEStl2YSujlNnlCy@cluster0.8na6esc.mongodb.net/?retryWrites=true&w=majority';
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+    const parsedUrl = parse(req.url!, true);
+    handle(req, res, parsedUrl);
+  }).listen(port);
 
   console.log(
     `> Server listening at http://localhost:${port} as ${
       dev ? 'development' : process.env.NODE_ENV
     }`
-  )
-})
+  );
+});
